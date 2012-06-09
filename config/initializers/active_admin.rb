bdda4500a09eac326f381c37790a38e43e359209
Nobuhiro <satonobuhiro@macintosh5-2.local>
2012-06-09 16:49:22 -0700

    item page CSS

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Barzit"
+  config.site_title = "BARZiT"
   config.site_title_link = :root
   
   # == Default Namespace
