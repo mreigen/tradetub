36dcbd25cbed26d9994f3734af5e87583a8cd1b8
Minh Reigen <minhtrietle@yahoo.com>
2012-06-17 00:51:59 -0700

    sab

--- db/fixtures/categories.rb
+++ db/fixtures/categories.rb
@@ -1,49 +1,59 @@
-Category.seed(:fullname) do |s|
-  s.fullname = "Antiques"
-  s.nickname = "antiques"
-end
-
-Category.seed(:fullname) do |s|
-  s.fullname = "Art"
+Category.seed(:nickname) do |s|
+  s.fullname = "Art & Collectibles & Crafts"
   s.nickname = "art"
 end
 
-Category.seed(:fullname) do |s|
-  s.fullname = "Baby"
-  s.nickname = "baby"
+Category.seed(:nickname) do |s|
+  s.fullname = "Fashion"
+  s.nickname = "fashion"
 end
 
-Category.seed(:fullname) do |s|
+Category.seed(:nickname) do |s|
   s.fullname = "Books"
   s.nickname = "books"
 end
 
-Category.seed(:fullname) do |s|
-  s.fullname = "Business & Industrial"
-  s.nickname = "bus_ind"
+Category.seed(:nickname) do |s|
+  s.fullname = "Sports & Outdoors"
+  s.nickname = "sports_outdoor"
 end
 
-Category.seed(:fullname) do |s|
-  s.fullname = "Collectibles"
-  s.nickname = "collectibles"
+Category.seed(:nickname) do |s|
+  s.fullname = "Automotive"
+  s.nickname = "motors"
 end
 
-Category.seed(:fullname) do |s|
+Category.seed(:nickname) do |s|
   s.fullname = "Computers/Tablets & Networking"
   s.nickname = "comp_tab_net"
 end
 
-Category.seed(:fullname) do |s|
-  s.fullname = "Consumer Electronics"
+Category.seed(:nickname) do |s|
+  s.fullname = "Electronics"
   s.nickname = "con_elec"
 end
 
-Category.seed(:fullname) do |s|
-  s.fullname = "Crafts"
-  s.nickname = "crafts"
+Category.seed(:nickname) do |s|
+  s.fullname = "Home & Decors"
+  s.nickname = "home_indoor"
 end
 
-Category.seed(:fullname) do |s|
-  s.fullname = "DVDs & Movies"
-  s.nickname = "dvd_mov"
+Category.seed(:nickname) do |s|
+  s.fullname = "Movie DVDs & Music CDs"
+  s.nickname = "dvd_cd"
+end
+
+Category.seed(:nickname) do |s|
+  s.fullname = "Hobbies & Toys"
+  s.nickname = "hobbies"
+end
+
+Category.seed(:nickname) do |s|
+  s.fullname = "Pet Supplies"
+  s.nickname = "pet"
+end
+
+Category.seed(:nickname) do |s|
+  s.fullname = "Video / PC Games"
+  s.nickname = "games"
 end
