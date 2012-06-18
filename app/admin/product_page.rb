8131a296dd8f8a04d21f49925ca0fde489c7f7de
Minh Reigen <minhtrietle@yahoo.com>
2012-06-17 18:17:35 -0700

    Front page: display only deleted and available.

--- app/admin/product_page.rb
+++ app/admin/product_page.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register_page "Product Page" do
   menu false
 
   content do
-    products = Product.all
+    products = Product.where("deleted == 'f' AND available == 't'")
     render :partial => "products/index", :locals => {:products => products}
   end
 end
