1989dbe4cb1f78900f8bc642fd89575a229a789d
Minh Reigen <minhtrietle@yahoo.com>
2012-07-01 00:33:37 -0700

    Image upload: use S3 only for production. #4

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -3,12 +3,14 @@ class ImageUpload < ActiveRecord::Base
   attr_accessor :delete_image
   attr_accessible :image
   has_attached_file :image, 
+=begin #production
     :storage => :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
+=end
     :styles => { :large => "520x380>", :medium => "238x238#", :thumb => "100x100#" }
   
   before_validation { :image.clear if delete_image == '1'}
